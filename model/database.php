37e87abb11e7a73690a6d0674a368d80d5c08c67
micodian <micodian83@gmail.com>
2021-02-25 21:00:55 -0600

    removed echo statement for connection

--- model/database.php
+++ model/database.php
@@ -26,7 +26,7 @@
         $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
         // set the PDO error mode to exception
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
+        //echo "Connected successfully";
         }
     catch(PDOException $e)
         {
